5aa1039438a2ccac18b73861ea8b8d3d54843970
Teddy Koker <teddy.koker@gmail.com>
2020-01-30 11:10:23 -0500

    added README

---
+++ README.md
@@ -0,0 +1,34 @@
+# Unsupervised Deep Homography - *Unoffical* PyTorch Implementation
+
+[**Unsupervised Deep Homography: A Fast and Robust Homography Estimation
+Model**](https://arxiv.org/abs/1709.03966)<br>
+Ty Nguyen, Steven W. Chen, Shreyas S. Shivakumar, Camillo J. Taylor, Vijay
+Kumar<br>
+
+
+![models](figures/models.jpg)
+*Figure from original paper. Proposed model is \(c\)*
+
+## Implementation Details
+
+This implementation leverages [kornia](https://github.com/kornia/kornia), an
+open source differentiable computer vision library. Kornia is used for computing
+the direct linear transform (DLT) as well as the spatial transformation.
+
+`model.py`: Regression model implementation <br>
+`dataset.py`: Synthetic data generator <br>
+`train.py`: Train unsupervised model using photometric loss outlined in paper
+
+
+### Training
+
+The model can be trained using synthetic data, created from the
+[COCO](http://cocodataset.org/) dataset. 
+
+```bash
+python train.py \
+    --bs=64 \            # batch size
+    --lr=1e-5 \          # learning rate
+    --epochs=100 \       # number of epochs
+    path/to/COCO/train/  # COCO training data
+```
--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
 
 from torch.utils.data import DataLoader, random_split
 from torch.utils.tensorboard import SummaryWriter
@@ -8,60 +6,7 @@ from tqdm import tqdm
 import kornia
 
 from dataset import SyntheticDataset
-
-device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
-
-BATCH_SIZE = 64
-LR = 1e-5
-
-DATA_PATH = "/home/thomas/Data/train2017"
-
-
-class Flatten(nn.Module):
-    def forward(self, x):
-        return x.view(x.size(0), -1)
-
-
-class Block(nn.Module):
-    def __init__(self, inchannels, outchannels, batch_norm=True):
-        super(Block, self).__init__()
-        layers = []
-        layers.append(nn.Conv2d(inchannels, outchannels, kernel_size=3, padding=1))
-        layers.append(nn.ReLU())
-        if batch_norm:
-            layers.append(nn.BatchNorm2d(outchannels))
-        layers.append(nn.Conv2d(outchannels, outchannels, kernel_size=3, padding=1))
-        layers.append(nn.ReLU())
-        if batch_norm:
-            layers.append(nn.BatchNorm2d(outchannels))
-        layers.append(nn.MaxPool2d(2, 2))
-        self.layers = nn.Sequential(*layers)
-
-    def forward(self, x):
-        return self.layers(x)
-
-
-class Net(nn.Module):
-    def __init__(self, batch_norm=True):
-        super(Net, self).__init__()
-        self.cnn = nn.Sequential(
-            Block(2, 64, batch_norm),
-            Block(64, 128, batch_norm),
-            Block(128, 256, batch_norm),
-            Block(256, 256, batch_norm),
-            Block(256, 256, batch_norm),
-            Flatten(),
-        )
-        self.fc = nn.Sequential(
-            nn.Linear(256 * 4 * 4, 4096), nn.ReLU(), nn.Linear(4096, 4 * 2),
-        )
-
-    def forward(self, a, b):
-        x = torch.cat((a, b), dim=1)  # combine two images in channel dimension
-        x = self.cnn(x)
-        x = self.fc(x)
-        delta = x.view(-1, 4, 2)
-        return delta
+from model import Net
 
 
 def photometric_loss(delta, img_a, img_b, points):
@@ -71,7 +16,7 @@ def photometric_loss(delta, img_a, img_b, points):
     return torch.mean(torch.abs(img_b_hat - img_b))
 
 
-def train_step(model, optimizer, dataloader, writer):
+def train_step(model, optimizer, dataloader, device, writer):
     model.train()
     total_loss = 0.0
     size = len(dataloader.dataset)
@@ -93,7 +38,7 @@ def train_step(model, optimizer, dataloader, writer):
     return total_loss / size
 
 
-def valid_step(model, dataloader):
+def valid_step(model, dataloader, device):
     model.eval()
     with torch.no_grad():
         total_loss = 0.0
@@ -113,6 +58,7 @@ def valid_step(model, dataloader):
 
 
 def fit(epochs):
+    device = torch.device("cuda:1" if torch.cuda.is_available() else "cpu")
     dataset = SyntheticDataset(DATA_PATH)
     train_size = int(0.8 * len(dataset))
     train_set, valid_set = random_split(
@@ -128,8 +74,8 @@ def fit(epochs):
     optimizer = torch.optim.Adam(model.parameters(), lr=LR)
 
     for e in range(epochs):
-        train_loss = train_step(model, optimizer, train_loader, writer)
-        valid_loss = valid_step(model, valid_loader)
+        train_loss = train_step(model, optimizer, train_loader, device, writer)
+        valid_loss = valid_step(model, valid_loader, device)
         print(f"{e}\t{train_loss:.4f}\t{valid_loss:.4f}")
         torch.save(model.state_dict(), f"model_{e}.pt")
 
@@ -153,5 +99,10 @@ def test_dataset():
         plt.show()
 
 
+BATCH_SIZE = 64
+LR = 1e-5
+DATA_PATH = "/home/thomas/Data/train2017"
+
+
 # test_dataset()
-fit(20)
+fit(100)
