344a045fedd937a9e8604ed4fc92428b1330672c
Teddy Koker <teddy.koker@gmail.com>
2020-03-23 14:39:45 -0400

    filetype arg

--- test.py
+++ test.py
@@ -18,7 +18,7 @@ def tensors_to_gif(a, b, name):
 def main(args):
     model = HomographyModel.load_from_checkpoint(args.checkpoint)
     model.eval()
-    test_set = SyntheticDataset(args.test_path, rho=args.rho)
+    test_set = SyntheticDataset(args.test_path, rho=args.rho, filetype=args.filetype)
 
     for i in range(args.n):
         img_a, patch_a, patch_b, corners, delta = test_set[i]
@@ -45,6 +45,7 @@ if __name__ == "__main__":
     parser.add_argument("--gpus", type=str, default="0")
     parser.add_argument("--rho", type=int, default=20, help="amount to perturb corners")
     parser.add_argument("--n", type=int, default=5, help="number of images to test")
+    parser.add_argument("--filetype", default=".jpg")
     parser.add_argument("test_path", help="path to test images")
     args = parser.parse_args()
     main(args)
